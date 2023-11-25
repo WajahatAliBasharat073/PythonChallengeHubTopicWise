0259f904a850e47c8eab5ed32cad74b653390c01
Wajahat Ali Basharat <83087637+WajahatAliBasharat073@users.noreply.github.com>
2023-11-26 02:21:42 +0500

    Excercise 1

---
+++ Introduction to Programming Exercises/Ex1_mailing_address.py
@@ -0,0 +1,9 @@
+# Exercise 1: Mailing Address
+# Create a program that displays your name and complete mailing address formatted in
+# # the manner that you would usually see it on the outside of an envelope
+print("Wajahat Ali Basharat")
+print("Department of Computer Science")
+print("University of Calgary")
+print("2500 University NW")
+print("Calgary, Alberta T2N ")
+print("Pakistan")
