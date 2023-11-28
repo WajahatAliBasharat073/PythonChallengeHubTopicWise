30fcdb4dd7e7a84e3b6efd33618c1a0c06f4a8a9
Wajahat Ali Basharat <83087637+WajahatAliBasharat073@users.noreply.github.com>
2023-11-28 23:09:47 +0500

    Excercise 1 Problem 3 Area of Ro om 11/28/2023

--- Introduction to Programming Exercises/p3_area_of_room.py
+++ Introduction to Programming Exercises/p3_area_of_room.py
@@ -4,17 +4,17 @@
 # room. The length and the width will be entered as floating point numbers. Include
 # units in your prompt and output message; either feet or meters, depending on which
 # unit you are more comfortable working with.
-# ChooseUnit=input("Enter the width and length of a room. For feet(1) or meters (2): ")
-# if ChooseUnit=="1":
-#     width=float(input("Enter the width of the room in feet: "))
-#     length=float(input("Enter the length of the room in feet: "))
-#     area=width*length
-#     print("The area of the room is",area,"square feet.")
-# elif ChooseUnit=="2":
-#     width=float(input("Enter the width of the room in meters: "))
-#     length=float(input("Enter the length of the room in meters: "))
-#     area=width*length
-#     print("The area of the room is",area,"square meters.")
+ChooseUnit=input("Enter the width and length of a room. For feet(1) or meters (2): ")
+if ChooseUnit=="1":
+    width=float(input("Enter the width of the room in feet: "))
+    length=float(input("Enter the length of the room in feet: "))
+    area=width*length
+    print("The area of the room is",area,"square feet.")
+elif ChooseUnit=="2":
+    width=float(input("Enter the width of the room in meters: "))
+    length=float(input("Enter the length of the room in meters: "))
+    area=width*length
+    print("The area of the room is",area,"square meters.")
     
 # Exercise 3: Area of a Room
 # **********************************************************************************************************************   
